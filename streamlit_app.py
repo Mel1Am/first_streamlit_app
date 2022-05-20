a6314174e11b9e916d64f004c10b85d30d3d7803
Mel1Am <96151732+Mel1Am@users.noreply.github.com>
2022-05-20 11:48:55 -0500

    Update streamlit_app.py
    - Fix dependencies errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.subtitle('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
-streamlit.body("Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard Boiled Free Range Egg")
+streamlit.text("Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard Boiled Free Range Egg")
