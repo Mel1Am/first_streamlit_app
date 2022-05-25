ed01747dde52021a898f8e37b76f2885b2332b55
Mel1Am <96151732+Mel1Am@users.noreply.github.com>
2022-05-25 10:17:10 -0500

    Update streamlit_app.py
    - Text Box added.
    - Ask customer to add fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information abou?', 'Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -52,5 +52,11 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 #streamlit.text("Hello from Snowflake:")
+
+#-----Adding a second Text entry Box----
+#Allow the end user to add a fruit to the list
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
 
 
