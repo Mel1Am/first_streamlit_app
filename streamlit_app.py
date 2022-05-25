1cf633424e2e79ea92e27667e63571c0412a019b
Mel1Am <96151732+Mel1Am@users.noreply.github.com>
2022-05-25 13:21:37 -0500

    Update streamlit_app.py
    - function to move the fruit load list query and load into a button action

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,18 +54,27 @@ except URLError as e:
   streamlit.error()
 
 #------------Query metadata account from Snowflake-----
-streamlit.stop() #Stop anything from running, just while we troubleshoot
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+'''my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("select * from fruit_load_list")
 #my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-#streamlit.text("Hello from Snowflake:")
+#streamlit.text("Hello from Snowflake:")'''
 
+streamlit.stop() #Stop anything from running, just while we troubleshoot
 #-----Adding a second Text entry Box----
 #Allow the end user to add a fruit to the list
 
